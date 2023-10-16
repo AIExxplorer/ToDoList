bc3bde4c903089521c62c02a3f3aa4a1d0e6610d
Wagner Rafael <contactaiexxplorer@gmail.com>
2023-10-16 19:02:02 -0300

    ToDoList - Projeto Inicial

---
+++ src/main/java/br/com/wagnerrafael/todolist/user/UserController.java
@@ -0,0 +1,50 @@
+package br.com.wagnerrafael.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Modificador
+ * public
+ * private
+ * protected
+ */ 
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    /**
+     * String (texto)
+     * Integer (int) números inteiros
+     * Double (double) números 0.0000
+     * Float (float) números 0.000
+     * char (A C )
+     * Date (data)
+     * Void (não ter retorno)
+     */
+    /* 
+     * Body
+     */
+     @Autowired
+     private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+       var user = this.userRepository.findByUsername(userModel.getUsername());
+
+       if(user !=null) {
+        // Mensagem de erro
+        // Status Code
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+       }
+
+       var userCreated = this.userRepository.save(userModel);
+       return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+}
