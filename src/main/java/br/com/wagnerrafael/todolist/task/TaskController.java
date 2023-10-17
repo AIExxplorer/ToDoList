310a07ab8ead4dac5464dbae61755ba22d2e9c85
Wagner Rafael <contactaiexxplorer@gmail.com>
2023-10-17 00:40:37 -0300

    Adição de restrições para requisição de nova tarefa a partir da data

--- src/main/java/br/com/wagnerrafael/todolist/task/TaskController.java
+++ src/main/java/br/com/wagnerrafael/todolist/task/TaskController.java
@@ -1,8 +1,12 @@
 package br.com.wagnerrafael.todolist.task;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +22,18 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         System.out.println("Chegou no controller");
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        // 10/11/2023 - Current
+        // 10/10/2023 - startAt
+        if(currentDate.isAfter(taskModel.getStartAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser maior do que a data atual");
+        }
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
