aaf67dcf2f8ae433856ca40b5c2cab0677c99ffe
Wagner Rafael <contactaiexxplorer@gmail.com>
2023-10-18 18:26:20 -0300

    Criação do método de reload automático da aplicação e configuração de mensagens de aviso e erro.

---
+++ src/main/java/br/com/wagnerrafael/todolist/errors/ExceptionHandlerController.java
@@ -0,0 +1,16 @@
+package br.com.wagnerrafael.todolist.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+    
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handletHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMostSpecificCause().getMessage());
+    }
+}
